2f99d737f7d5673cef86245fc5b97d524de93986
adamisrusty <adamgutierreztx@gmail.com>
2020-11-04 20:06:44 -0600

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+class Trapezoid {
+    private:
+        double height;
+        double width;
+};
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
